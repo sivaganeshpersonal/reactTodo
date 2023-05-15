a280bc24b5a736b0f27ac12d7836dc3969ec8c23
sivaganeshpersonal <124411773+sivaganeshpersonal@users.noreply.github.com>
2023-05-15 07:03:12 +0530

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function ConfirmModel(props) {
+  const [show, setShow] = useState(true);
+
+  const handleClose = () => props.onClose(true);
+  const handleDismiss = () => props.onClose(false);
+
+  return (
+    <>
+      <Modal show={show} onHide={handleDismiss}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirmation required</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete this todo!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleDismiss}>
+            No
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Yes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+export default ConfirmModel;
